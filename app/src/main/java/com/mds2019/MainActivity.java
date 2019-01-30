a591c954dc0ab31b3b091b913c95374395c4475a
ysminskmd <ys.minsk.md@yandex.ru>
2019-01-30 03:44:31 +0300

    feature: add appcenter

--- app/src/main/java/com/mds2019/MainActivity.java
+++ app/src/main/java/com/mds2019/MainActivity.java
@@ -5,17 +5,24 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import com.crashlytics.android.Crashlytics;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.distribute.Distribute;
+
 import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String APP_CENTER_KEY = "7e27690d-f12f-41d0-b9cc-71fa53763ba7";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
 
         final TextView version = findViewById(R.id.version);
         version.setText(BuildConfig.VERSION_NAME);
+
+        Fabric.with(this, new Crashlytics());
+        AppCenter.start(getApplication(), APP_CENTER_KEY, Distribute.class);
     }
 }

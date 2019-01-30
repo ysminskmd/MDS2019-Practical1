82e74cad498ef0b19cc3b7768b7c1ba5fcbde8f8
ysminskmd <ys.minsk.md@yandex.ru>
2019-01-30 03:20:57 +0300

    feature: add app version name

--- app/src/main/java/com/mds2019/MainActivity.java
+++ app/src/main/java/com/mds2019/MainActivity.java
@@ -2,6 +2,8 @@ package com.mds2019;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
 import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
 
@@ -12,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
+
+        final TextView version = findViewById(R.id.version);
+        version.setText(BuildConfig.VERSION_NAME);
     }
 }
